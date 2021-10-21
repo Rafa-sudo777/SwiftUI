1a3b04e8429cd9816f4a2dc4a77ad0938f10898e
Rafael <rafaelaviles10@hotmail.com>
2021-10-20 19:54:17 -0500

    Initial Commit

---
+++ ObservedObject vs StateObject/ObservedObject_vs_StateObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservedObject_vs_StateObjectApp.swift
+//  ObservedObject vs StateObject
+//
+//  Created by Rafael Aviles Puebla on 20/10/21.
+//
+
+import SwiftUI
+
+@main
+struct ObservedObject_vs_StateObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
